d7b7ef1b9a353649a83a5c0a63fa3f5bd79c59ff
Graeme Rocher <graeme.rocher@gmail.com>
2016-06-01 20:31:30 +0200

    Fix plugin build and add Gradle plugin class

---
+++ src/main/groovy/org/grails/gdoc/asciidoc/GdocToAsciiDocConverterPlugin.groovy
@@ -0,0 +1,16 @@
+package org.grails.gdoc.asciidoc
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by graemerocher on 01/06/2016.
+ */
+@CompileStatic
+class GdocToAsciiDocConverterPlugin implements Plugin<Project>{
+    @Override
+    void apply(Project project) {
+        // TODO
+    }
+}
