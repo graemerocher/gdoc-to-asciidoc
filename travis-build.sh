909188b0ca8fb8fb26493e02fba32cc7e5c26214
Graeme Rocher <graeme.rocher@gmail.com>
2016-06-02 18:06:24 +0200

    Add Travis setup

---
+++ travis-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+./gradlew clean test assemble
+
+EXIT_STATUS=0
+echo "Publishing archives for branch $TRAVIS_BRANCH"
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+  echo "Publishing archives"
+
+  if [[ -n $TRAVIS_TAG ]]; then
+      ./gradlew bintrayUpload || EXIT_STATUS=$?
+  fi
+
+fi
+
+exit $EXIT_STATUS
